aab1461227013bcbdd42712503e878b1a12ef18b
zyunx <zyunx@zyunx.net>
2017-08-01 17:57:30 +0800

    add some comment in javacommons-api

--- javacommons-api/src/main/java/net/zyunx/javacommons/api/ApiReq.java
+++ javacommons-api/src/main/java/net/zyunx/javacommons/api/ApiReq.java
@@ -1,5 +1,17 @@
+/* 
+ * ApiReq.java
+ * javacommmons-api  Copyright (C) 2017  张云
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; see LICENCE for details.
+ */
 package net.zyunx.javacommons.api;
 
+/**
+ * API request object
+ * @author zyunx 201700801
+ *
+ */
 public class ApiReq {
 
 }
--- javacommons-api/src/main/java/net/zyunx/javacommons/api/ApiRsp.java
+++ javacommons-api/src/main/java/net/zyunx/javacommons/api/ApiRsp.java
@@ -1,12 +1,38 @@
+/* 
+ * ApiRsp.java
+ * javacommmons-api  Copyright (C) 2017  张云
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; see LICENCE for details.
+ */
 package net.zyunx.javacommons.api;
 
-import java.io.Serializable;
-
+/**
+ * API response object
+ * @author zyunx 201700801
+ *
+ */
 public class ApiRsp<T> {
 	
 	private String code;
 	
 	private T data;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
 	
 	
 }
